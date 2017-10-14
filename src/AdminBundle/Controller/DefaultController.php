bb4a7589ebcc3c8c9c55824b6cf37e0537b7ede4
Snaiky11 <snaiky@protonmail.com>
2017-10-14 21:07:07 +0200

    Initial commit

---
+++ src/AdminBundle/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('AdminBundle::index.html.twig');
+    }
+}
